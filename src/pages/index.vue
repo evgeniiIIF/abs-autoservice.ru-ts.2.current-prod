b9a7ff03b308bfbac7a0ad358eb7541c572492cb
anvyden <anvyden@gmail.com>
2023-12-01 00:38:40 +0700

    fix UI-input

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-  const name = ref('')
-  const phone = ref('')
 </script>
 
 <template>
-  <div><UIInput type="text" title="Тест" :placeholder="123" v-model="name"/>
-    <UIInput type="phone" title="Тест" v-model="phone"/></div>
+  <div></div>
 </template>
 
 <style lang="scss"></style>
