8e0d979ead44cb3b50e8d8db019a2fce025c5c36
anvyden <anvyden@gmail.com>
2023-12-01 00:32:34 +0700

    create UI-input

--- src/components/UIInput/UIInput.vue
+++ src/components/UIInput/UIInput.vue
@@ -15,23 +15,23 @@ const onInputHandler = (event: Event): string => {
     <label class="input__label">
       <span class="input__title">{{ title }}</span>
       <input
-          v-if="type === 'phone'"
-          v-maska
-          class="input__input"
-          :data-maska="'+7 (###) ###-##-##'"
-          type="tel"
-          placeholder="+7 (___) __-__-__"
-          :value="modelValue"
-          v-bind="$attrs"
-          @input="$emit('update:modelValue', onInputHandler($event))"
+        v-if="type === 'phone'"
+        v-maska
+        class="input__input"
+        :data-maska="'+7 (###) ###-##-##'"
+        type="tel"
+        placeholder="+7 (___) __-__-__"
+        :value="modelValue"
+        v-bind="$attrs"
+        @input="$emit('update:modelValue', onInputHandler($event))"
       />
       <input
-          v-else
-          class="input__input"
-          :type="type"
-          :value="modelValue"
-          v-bind="$attrs"
-          @input="$emit('update:modelValue', onInputHandler($event))"
+        v-else
+        class="input__input"
+        :type="type"
+        :value="modelValue"
+        v-bind="$attrs"
+        @input="$emit('update:modelValue', onInputHandler($event))"
       />
       <span v-if="errorMessage" class="input__error">{{ errorMessage }}</span>
     </label>
@@ -39,48 +39,48 @@ const onInputHandler = (event: Event): string => {
 </template>
 
 <style lang="scss">
-  .input {
-    position: relative;
+.input {
+  position: relative;
 
-    &__title {
-      @include BodySRegular;
-      color: var(--black-black-40, #999);
+  &__title {
+    @include BodySRegular;
+    color: var(--black-black-40, #999);
+  }
+
+  &__input {
+    width: 100%;
+    padding: 12px 16px;
+    @include BodyMRegular;
+    color: var(--black-black-20, #cbcccc);
+    background-color: var(--black-black-60, #717171);
+    border-radius: 10px;
+    outline: none;
+    border: none;
+
+    &::placeholder {
+      color: var(--black-black-20, #cbcccc);
     }
+  }
 
-    &__input {
-      width: 100%;
-      padding: 12px 16px;
-      @include BodyMRegular;
-      color: var(--black-black-20, #CBCCCC);
-      background-color: var(--black-black-60, #717171);
-      border-radius: 10px;
-      outline: none;
-      border: none;
+  &__error {
+    position: absolute;
+    bottom: -16px;
+    left: 0;
 
-      &::placeholder {
-        color: var(--black-black-20, #CBCCCC);
+    @include BodyXSRegular;
+    color: red;
+  }
+
+  &--error {
+    .input {
+      &__title {
+        color: red;
       }
-    }
 
-    &__error {
-      position: absolute;
-      bottom: -16px;
-      left: 0;
-
-      @include BodyXSRegular;
-      color: red;
-    }
-
-    &--error {
-      .input {
-        &__title {
-          color: red;
-        }
-
-        &__input {
-          background-color: indianred;
-        }
+      &__input {
+        background-color: indianred;
       }
     }
   }
+}
 </style>
