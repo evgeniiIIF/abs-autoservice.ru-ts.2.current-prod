e1e2ef6b836df475b13f4a1d2eb506ef1bb9aa59
anvyden <anvyden@gmail.com>
2023-12-04 20:47:52 +0700

    create about-company

--- src/components/AboutCompany/AboutCompany.vue
+++ src/components/AboutCompany/AboutCompany.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-  import {useMediaSizes} from "@/composables/useMediaSizes";
+import { useMediaSizes } from '@/composables/useMediaSizes';
 
-  const descriptionIsOpen = ref(false);
-  const descriptionRef = ref<HTMLDivElement | null>(null)
-  const descriptionHasFixedHeight = ref(false);
+const descriptionIsOpen = ref(false);
+const descriptionRef = ref<HTMLDivElement | null>(null);
+const descriptionHasFixedHeight = ref(false);
 
-  const { isDesktop } = useMediaSizes();
+const { isDesktop } = useMediaSizes();
 
-  watch(() => [descriptionRef.value, isDesktop.value], () => {
-    descriptionHasFixedHeight.value = (descriptionRef.value.clientHeight >= 88) && !isDesktop.value
-    console.log(descriptionRef.value.clientHeight)
-  })
+watch(
+  () => [descriptionRef.value, isDesktop.value],
+  () => {
+    descriptionHasFixedHeight.value = descriptionRef.value.clientHeight >= 88 && !isDesktop.value;
+    console.log(descriptionRef.value.clientHeight);
+  },
+);
 </script>
 
 <template>
@@ -27,15 +30,29 @@
             </div>
           </li>
         </ul>
-        <div class="about-company__description" ref="descriptionRef" :class="{ 'about-company__description--fixed-height': descriptionHasFixedHeight, 'about-company__description--opened': descriptionIsOpen }" @click="() => console.log(descriptionRef.clientHeight)">
+        <div
+          ref="descriptionRef"
+          class="about-company__description"
+          :class="{
+            'about-company__description--fixed-height': descriptionHasFixedHeight,
+            'about-company__description--opened': descriptionIsOpen,
+          }"
+          @click="() => console.log(descriptionRef.clientHeight)"
+        >
           Автосервис ABS-AUTO – сервисный центр в Ставрополе, который входит в состав торгово-инвестиционной группы
           компаний ABS GROUP. Холдинг является одним из крупнейших поставщиков автомобильных запчастей и смазочных
-          материалов в СНГ, занимается производством собственных запчастей под брендом ABSEL, а также создает современные
-          технологичные решения для автобизнеса. В автосервисе ABS-AUTOмы собрали команду настоящих профессионалов,
-          которых объединяет искренняя любовь к автомобилям. Мы предлагаем комплексные услуги по ремонту и полноценному
-          обслуживанию Вашего авто с применением только высококачественных средств и оборудования.
+          материалов в СНГ, занимается производством собственных запчастей под брендом ABSEL, а также создает
+          современные технологичные решения для автобизнеса. В автосервисе ABS-AUTOмы собрали команду настоящих
+          профессионалов, которых объединяет искренняя любовь к автомобилям. Мы предлагаем комплексные услуги по ремонту
+          и полноценному обслуживанию Вашего авто с применением только высококачественных средств и оборудования.
         </div>
-        <button v-if="descriptionHasFixedHeight" class="about-company__button" :class="{ 'about-company__button--close': descriptionIsOpen }" type="button" @click="descriptionIsOpen = !descriptionIsOpen">
+        <button
+          v-if="descriptionHasFixedHeight"
+          class="about-company__button"
+          :class="{ 'about-company__button--close': descriptionIsOpen }"
+          type="button"
+          @click="descriptionIsOpen = !descriptionIsOpen"
+        >
           <span>{{ descriptionIsOpen ? 'Свернуть' : 'Читать далее' }}</span>
           <IcArrowDown :font-controlled="false" :filled="true" />
         </button>
@@ -52,20 +69,20 @@
     display: grid;
     grid-template-columns: auto;
     grid-template-areas:
-    "title"
-    "subtitle"
-    "description"
-    "desc-button"
-    "reviews";
+      'title'
+      'subtitle'
+      'description'
+      'desc-button'
+      'reviews';
 
     @include desktop {
       justify-content: space-between;
       column-gap: 20px;
       grid-template-areas:
-        "title description"
-        "subtitle description"
-        "desc-button description"
-        "reviews description";
+        'title description'
+        'subtitle description'
+        'desc-button description'
+        'reviews description';
     }
   }
 
@@ -81,11 +98,11 @@
     }
   }
 
-  &__subtitle{
+  &__subtitle {
     grid-area: subtitle;
     margin-bottom: 20px;
     @include SubtitleSBold;
-    color: var(--green-primary, #00A19C);
+    color: var(--green-primary, #00a19c);
 
     @include desktop {
       @include TitleXSBold;
@@ -106,7 +123,7 @@
       align-items: center;
       padding: 16px;
       gap: 16px;
-      background: var(--linear, linear-gradient(180deg, rgba(42, 42, 42, 0.00) 0%, rgba(42, 42, 42, 0.40) 100%));
+      background: var(--linear, linear-gradient(180deg, rgba(42, 42, 42, 0) 0%, rgba(42, 42, 42, 0.4) 100%));
       border: 1px solid var(--black-black-90, #2a2a2a);
       border-radius: 20px;
 
