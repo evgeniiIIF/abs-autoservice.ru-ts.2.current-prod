45a910189da9e396999ce78178188d2f98829f83
anvyden <anvyden@gmail.com>
2023-12-04 18:28:01 +0700

    process about-comapny

--- src/components/AboutCompany/AboutCompany.vue
+++ src/components/AboutCompany/AboutCompany.vue
@@ -13,10 +13,29 @@
         <li class="about-company__review-service">
           <NuxtPicture src="/images/yandex-review.png" loading="lazy" alt="Яндекс" />
           <ul class="about-company__review-service-rating">
-            <li class="about-company__review-service-star"></li>
+            <li v-for="star in 5" :key="star" class="about-company__review-service-star">
+              <IcStar :font-controlled="false" :filled="true" />
+            </li>
           </ul>
         </li>
       </ul>
+      <div class="about-company__description">
+        Автосервис ABS-AUTO – сервисный центр в Ставрополе, который входит в состав торгово-инвестиционной группы
+        компаний ABS GROUP. Холдинг является одним из крупнейших поставщиков автомобильных запчастей и смазочных
+        материалов в СНГ, занимается производством собственных запчастей под брендом ABSEL, а также создает современные
+        технологичные решения для автобизнеса. В автосервисе ABS-AUTOмы собрали команду настоящих профессионалов,
+        которых объединяет искренняя любовь к автомобилям. Мы предлагаем комплексные услуги по ремонту и полноценному
+        обслуживанию Вашего авто с применением только высококачественных средств и оборудования.
+      </div>
+      <button class="about-company__button" type="button">
+        <span>Читать далее</span>
+        <IcArrowDown :font-controlled="false" :filled="true" />
+      </button>
     </div>
   </section>
 </template>
+
+<style lang="scss">
+.about-company {
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div><AboutCompany /></div>
 </template>
 
 <style lang="scss"></style>
