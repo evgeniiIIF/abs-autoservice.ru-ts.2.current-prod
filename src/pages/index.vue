cae72f21a09602bceab40483f01ca2a630a916cf
anvyden <anvyden@gmail.com>
2023-12-04 22:11:39 +0700

    add another one company picture. Fix paddings

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,7 @@
     <CompanyPicture src="/images/company-1.png" alt="Ресепшен" />
     <PopularServices />
     <CallbackForm />
-    <CompanyPicture src="/images/company-2.png" alt="Зал автосервиса"/>
+    <CompanyPicture src="/images/company-2.png" alt="Зал автосервиса" />
     <AboutCompany />
   </div>
 </template>
