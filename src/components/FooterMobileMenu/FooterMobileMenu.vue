d04a558348ba0c70c4649cd8df3b2dce240e72cc
dk.gusyatnikov <dimagusi111@gmail.com>
2024-01-12 12:22:34 +0300

    fix menu mobile

--- src/components/FooterMobileMenu/FooterMobileMenu.vue
+++ src/components/FooterMobileMenu/FooterMobileMenu.vue
@@ -4,36 +4,68 @@ import { useContactsStore } from '~/store/contacts';
 
 const { contactsState } = useContactsStore();
 
-const [isCAllbackOpenModal, openCAllbackModal, closeCallbackModal] = useBooleanState();
-const [isOpenSearchModal, openSearchModal, closeSearchModal] = useBooleanState();
+const { y, directions } = useScroll(window);
 
-const socialWhatsappItem = computed(() => {
-  return contactsState.value.data?.social_network?.find((item) => item.title === 'Whatsapp');
+const [isOpenSearchModal, openSearchModal, closeSearchModal] = useBooleanState();
+const [isOpenContactsMenu, openContactsMenu, closeContactsMenu] = useBooleanState();
+const [isShowTooltip, showTooltip, hideTooltip] = useBooleanState(false);
+
+watch(y, () => {
+  if (directions.top) hideTooltip();
+  if (directions.bottom) showTooltip();
 });
 </script>
 
 <template>
   <div ref="menuRef" class="footer-mobile-menu js-footer-mobile-menu">
-    <ul class="footer-mobile-menu__list">
-      <li class="footer-mobile-menu__item" @click="openCAllbackModal">
+    <ul v-if="!isOpenContactsMenu" class="footer-mobile-menu__list">
+      <li class="footer-mobile-menu__item" @click.stop="openContactsMenu">
         <IcChat :font-controlled="false" :filled="true" />
+        <div
+          :class="['footer-mobile-menu__item-tooltip', { 'footer-mobile-menu__item-tooltip--active': isShowTooltip }]"
+        >
+          Написать
+        </div>
       </li>
       <li class="footer-mobile-menu__item" @click="openSearchModal">
         <IcSearch :font-controlled="false" :filled="true" />
+        <div
+          :class="['footer-mobile-menu__item-tooltip', { 'footer-mobile-menu__item-tooltip--active': isShowTooltip }]"
+        >
+          Поиск
+        </div>
       </li>
       <li class="footer-mobile-menu__item">
         <NuxtLink :to="appRoutes.contacts()">
           <IcNavigation :font-controlled="false" :filled="true" />
         </NuxtLink>
+        <div
+          :class="['footer-mobile-menu__item-tooltip', { 'footer-mobile-menu__item-tooltip--active': isShowTooltip }]"
+        >
+          Контакты
+        </div>
       </li>
       <li class="footer-mobile-menu__item">
-        <a :href="socialWhatsappItem?.url" target="_blank" rel="noopener noreferrer">
-          <!-- <IcWhatsapp :font-controlled="false" :filled="true" /> -->
-          <img :src="socialWhatsappItem?.icon_social" :alt="socialWhatsappItem?.title" />
+        <NuxtLink :to="appRoutes.services()">
+          <IcStar :font-controlled="false" :filled="true" />
+        </NuxtLink>
+        <div
+          :class="['footer-mobile-menu__item-tooltip', { 'footer-mobile-menu__item-tooltip--active': isShowTooltip }]"
+        >
+          Услуги
+        </div>
+      </li>
+    </ul>
+    <ul v-if="isOpenContactsMenu" class="footer-mobile-menu__list">
+      <li class="footer-mobile-menu__item" @click.stop="closeContactsMenu">
+        <IcClose :font-controlled="false" :filled="true" />
+      </li>
+      <li v-for="contact in contactsState.data?.social_network" :key="contact.url" class="footer-mobile-menu__item">
+        <a :href="contact?.url" target="_blank" rel="noopener noreferrer">
+          <img :src="contact?.icon_social" :alt="contact?.title" />
         </a>
       </li>
     </ul>
-    <CallbackFormModal :is-open="isCAllbackOpenModal" @on-close="closeCallbackModal" />
     <AppSearchServiceModal :is-open="isOpenSearchModal" @on-close="closeSearchModal" />
   </div>
 </template>
@@ -45,6 +77,7 @@ const socialWhatsappItem = computed(() => {
   bottom: 0;
   width: 100%;
   z-index: $z-header;
+  min-height: 60px;
 
   border-radius: 10px 10px 0 0;
   border-top: 1px solid var(--black-black-80, #414141);
@@ -59,8 +92,30 @@ const socialWhatsappItem = computed(() => {
     width: 25%;
     display: flex;
     justify-content: center;
+    flex-wrap: wrap;
     align-items: center;
     padding: 12px 0;
+
+    svg {
+      @include svg-color(var(--white));
+      width: 30px;
+      height: 32px;
+    }
+  }
+
+  &__item-tooltip {
+    width: 100%;
+    text-align: center;
+    color: var(--white);
+    height: 0px;
+    overflow: hidden;
+    transition: 0.3s all;
+
+    @include BodyXSRegular;
+
+    &--active {
+      height: 18px;
+    }
   }
 }
 </style>
